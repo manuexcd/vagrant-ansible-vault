57bd89d7752d11120a12d5b9bbd2b966219ed0eb
Manuel Lara <manuexcd@gmail.com>
2019-07-24 12:45:22 +0200

    Primer commit

---
+++ src/main/java/com/at/vagrantansiblevault/VagrantAnsibleVaultApplication.java
@@ -0,0 +1,13 @@
+package com.at.vagrantansiblevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VagrantAnsibleVaultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VagrantAnsibleVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/at/vagrantansiblevault/VagrantAnsibleVaultApplicationTests.java
@@ -0,0 +1,16 @@
+package com.at.vagrantansiblevault;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VagrantAnsibleVaultApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
